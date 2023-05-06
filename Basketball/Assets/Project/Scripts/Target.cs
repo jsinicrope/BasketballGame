49c7861e302db1c046d81badbf06ec94de1231d6
jsinicrope <72351894+jsinicrope@users.noreply.github.com>
2023-05-06 15:09:36 -0400

    Level2Complete

---
+++ Basketball/Assets/Project/Scripts/Target.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private float min = 2f;
+    private float max = 3f;
+    public float dist = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        min = transform.position.z;
+        max = transform.position.z + dist;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time * 2, max - min) + min);
+    }
+}
