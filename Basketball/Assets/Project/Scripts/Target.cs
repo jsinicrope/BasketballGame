2caa6b091d4b824c8ed63eb037ca62d5a1310375
jsinicrope <72351894+jsinicrope@users.noreply.github.com>
2023-05-07 16:53:23 -0400

    wip_movement

--- Basketball/Assets/Project/Scripts/Target.cs
+++ Basketball/Assets/Project/Scripts/Target.cs
@@ -7,6 +7,9 @@ public class Target : MonoBehaviour
     private float min = 0f;
     private float max = 0f;
     public float dist = 5f;
+    public bool canMoveX = false;
+    public bool canMoveY = false;
+    public bool canMoveZ = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,5 +21,51 @@ public class Target : MonoBehaviour
     void Update()
     {
         transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time * 2, max - min) + min);
+        /*
+        if (canMoveX)
+        {
+            xMovement();
+        }
+
+        if (canMoveY)
+        {
+            yMovement();
+        }
+
+        if (canMoveZ)
+        {
+            zMovement();
+        }
+        */
+    }
+
+    void xMovement()
+    {
+        min = transform.position.x;
+        max = transform.position.x + dist;
+        while (canMoveX) 
+        { 
+        transform.position = new Vector3(Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.y, transform.position.z); 
+        }
+    }
+
+    void yMovement()
+    {
+        min = transform.position.y;
+        max = transform.position.y + dist;
+        while (canMoveY)
+        {
+            transform.position = new Vector3(transform.position.x, Mathf.PingPong(Time.time * 2, max - min) + min, transform.position.z);
+        }
+    }
+
+    void zMovement()
+    {
+        min = transform.position.z;
+        max = transform.position.z + dist;
+        while (canMoveY)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.PingPong(Time.time * 2, max - min) + min);
+        }
     }
 }
