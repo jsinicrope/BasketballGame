a4d1bbaada69a392e7d0fc84b954f8670f0c40d6
jsinicrope <72351894+jsinicrope@users.noreply.github.com>
2023-05-07 10:19:41 -0400

    mouse

--- Basketball/Assets/Project/Scripts/Player.cs
+++ Basketball/Assets/Project/Scripts/Player.cs
@@ -16,6 +16,8 @@ public class Player : MonoBehaviour
     void Start()
     {
         ball.GetComponent<Rigidbody>().useGravity = false;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
